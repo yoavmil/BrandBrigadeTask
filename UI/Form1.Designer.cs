9df30f019778d3a72049d390843b89994d9013b8
yoavmil <yoavmil@gmail.com>
2023-02-20 22:34:29 +0200

    webcam stream works

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -28,19 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webcamBtn = new System.Windows.Forms.Button();
+            this.fileBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(879, 401);
+            this.panel1.TabIndex = 0;
+            // 
+            // webcamBtn
+            // 
+            this.webcamBtn.Location = new System.Drawing.Point(12, 424);
+            this.webcamBtn.Name = "webcamBtn";
+            this.webcamBtn.Size = new System.Drawing.Size(75, 23);
+            this.webcamBtn.TabIndex = 1;
+            this.webcamBtn.Text = "Webcam";
+            this.webcamBtn.UseVisualStyleBackColor = true;
+            // 
+            // fileBtn
+            // 
+            this.fileBtn.Location = new System.Drawing.Point(93, 424);
+            this.fileBtn.Name = "fileBtn";
+            this.fileBtn.Size = new System.Drawing.Size(75, 23);
+            this.fileBtn.TabIndex = 1;
+            this.fileBtn.Text = "File";
+            this.fileBtn.UseVisualStyleBackColor = true;
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(174, 424);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(903, 459);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.fileBtn);
+            this.Controls.Add(this.webcamBtn);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Brand Brigade Task";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button webcamBtn;
+        private Button fileBtn;
+        private Button stopBtn;
     }
 }
--- UI/Form1.cs
+++ UI/Form1.cs
@@ -1,10 +1,129 @@
+using MFORMATSLib;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 namespace UI
 {
     public partial class Form1 : Form
     {
+        private MFPreviewClass m_objPreview;
+        private MFLiveClass m_objLive;
+        private Thread m_threadWorker;	//Working thread
+        private bool m_bWork;
+        private CancellationTokenSource cancelSource;
+        private int m_webcamIdx = -1;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_objPreview = new MFPreviewClass();
+            try
+            {
+                m_objLive = new MFLiveClass();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("MLive device not found\n\n" + ex.ToString());
+            }
+            //Configure preview
+            m_objPreview.PreviewWindowSet("", panel1.Handle.ToInt32());
+            m_objPreview.PreviewEnable("", 1, 1);
+
+            //Fill live devices
+            FindWebCamDeviceIdx();
+            SelectWebCam();
+            initWebCam();
+
+            // Create & config background worker
+            cancelSource = new CancellationTokenSource();
+            m_threadWorker = new Thread(() => thread_DoWork(cancelSource.Token));
+            m_threadWorker.Name = "thread_DoWork";
+            m_threadWorker.Start();
+
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;   //Need for use renderersCombobox in multithreading 
+
+        }
+
+        private void FindWebCamDeviceIdx()
+        {
+            int vCount;
+            try
+            {
+                m_objLive = new MFLiveClass();
+
+                m_objLive.DeviceGetCount(eMFDeviceType.eMFDT_Video, out vCount);
+
+                for (int i = 0; i < vCount; i++)
+                {
+                    string strName;
+                    int _pbBusy;
+                    m_objLive.DeviceGetByIndex(eMFDeviceType.eMFDT_Video, i, out strName, out _pbBusy);
+                    Debug.WriteLine(strName);
+                    if (strName.Contains("WebCam", StringComparison.OrdinalIgnoreCase))
+                        m_webcamIdx = i;
+                }
+            }
+            catch { }
+        }
+
+        private void SelectWebCam()
+        {
+            try
+            {
+                m_objLive.DeviceSet(eMFDeviceType.eMFDT_Video, m_webcamIdx, "");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void initWebCam()
+        {
+            // Set device
+            m_objLive.DeviceSet(eMFDeviceType.eMFDT_Video, m_webcamIdx, "");
+
+            // Set format
+            M_VID_PROPS vidProps;
+            string name;
+            m_objLive.FormatVideoGetByIndex(eMFormatType.eMFT_Input, m_webcamIdx, out vidProps, out name);
+            m_objLive.FormatVideoSet(eMFormatType.eMFT_Input, vidProps);
+
+            // Start worker
+            m_bWork = true;
+        }
+
+        private void thread_DoWork(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (m_bWork)
+                {
+                    MFFrame pFrame = null;
+
+                    try
+                    {
+                        m_objLive.SourceFrameGet(-1, out pFrame, "");
+
+                        //Send frame to the preview
+                        m_objPreview.ReceiverFramePut(pFrame, -1, "");
+
+                        //Release frame - DO NOT FORGOT TO DO THIS !!!
+                        Marshal.ReleaseComObject(pFrame);
+                    }
+                    catch
+                    {
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
     }
 }
